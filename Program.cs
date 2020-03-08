b6b157f74f858ead39d2ef403d070488df7d604d
maxson-AT191 <maxlenaruss123@gmail.com>
2020-03-08 22:38:04 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double a, b;
+            Console.WriteLine("Введите первое число: ");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Введите второе число: ");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            double result1 = (a * b);
+            double result2 = Math.Pow(result1, 2);
+            Console.WriteLine("Результат: " + result2);
+        }
+    }
+}
